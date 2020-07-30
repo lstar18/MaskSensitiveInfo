0fd3cf3f9f0a8ebc7ce3e68767eb2b93d3e6f17f
Liza Star <lizadstar18@gmail.com>
2020-07-29 19:26:10 -0500

    Add project files.

---
+++ MaskSensitiveInfo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace MaskSensitiveInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a secret here: ");
+            var secret = Console.ReadLine();
+
+            var secretInputArray = secret.ToCharArray();
+
+            int BeforeMaskedLetters = secret.Length - 5;
+            //Console.WriteLine(BeforeMaskedLetters);
+            if (BeforeMaskedLetters >= 0)
+            {
+                for (int i = 0; i < secretInputArray.Length - 4; i++)
+                {
+                    secretInputArray[i] = '#';
+                }
+            }
+            else
+            {
+                for (int i = 0; i < secretInputArray.Length; i++)
+                {
+                    secretInputArray[i] = '*';
+                }
+            }
+            var finalSecret = string.Join("", secretInputArray.Select(word => word.ToString()).ToArray());
+
+            Console.WriteLine($"Here is your secret word: {finalSecret}.");
+        }
+    }
+}
